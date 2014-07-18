585cf97e5beb556c843236800493074b438aab83
Zack Foster <zfoster@clearslide.com>
2014-07-17 21:09:44 -0700

    select image from gallery

--- src/com/lysterr/Lysterr/postflow/steps/PostFlowConfirmFragment.java
+++ src/com/lysterr/Lysterr/postflow/steps/PostFlowConfirmFragment.java
@@ -3,6 +3,7 @@ package com.lysterr.Lysterr.postflow.steps;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
--- src/com/lysterr/Lysterr/postflow/steps/PostFlowImageFragment.java
+++ src/com/lysterr/Lysterr/postflow/steps/PostFlowImageFragment.java
@@ -2,6 +2,8 @@ package com.lysterr.Lysterr.postflow.steps;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -12,13 +14,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import com.lysterr.Lysterr.R;
-import com.lysterr.Lysterr.postflow.model.NewGenericPostModel;
 import com.lysterr.Lysterr.postflow.PostFlowNavFragment;
 import com.lysterr.Lysterr.postflow.model.NewPostModel;
 import com.lysterr.Lysterr.util.DebugUtil;
 import com.lysterr.Lysterr.util.UiUtil;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -28,6 +30,7 @@ import java.util.Date;
  */
 public class PostFlowImageFragment extends Fragment {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_PICKER = 2;
 
     private String mCurrentPhotoPath;
 
@@ -50,21 +53,47 @@ public class PostFlowImageFragment extends Fragment {
             }
         });
 
+        button = (Button)view.findViewById(R.id.pick_image_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchPickPictureIntent();
+            }
+        });
+
         return view;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent dataIntent) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-//            Bundle extras = dataIntent.getExtras();
-//            // this is only a thumbnail, the full image was written to the file we gave in the intent
-//            // the path should be stored in mCurrentPhotoPath and should be passed along
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-
             NewPostModel data = (NewPostModel)getArguments().getSerializable(PostFlowNavFragment.ARG_DATA);
 
             // TODO: validate file exists and has non-zero length?
             data.setImagePath(mCurrentPhotoPath);
+            PostFlowNavFragment.notifyStepComplete(this, data);
+        } else if (requestCode == REQUEST_IMAGE_PICKER && resultCode == Activity.RESULT_OK) {
+            Uri _uri = dataIntent.getData();
+
+            try {
+                Bitmap bmp = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), _uri);
+                String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/lysterr";
+                File dir = new File(file_path);
+                if(!dir.exists())
+                    dir.mkdirs();
+                File file = new File(dir, "image" + ".jpeg");
+                FileOutputStream fOut = new FileOutputStream(file);
+
+                bmp.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+                fOut.flush();
+                fOut.close();
+            } catch (IOException e) {
+                DebugUtil.debugException(e);
+            }
+
+            NewPostModel data = (NewPostModel)getArguments().getSerializable(PostFlowNavFragment.ARG_DATA);
+            data.setImagePath(Environment.getExternalStorageDirectory().getAbsolutePath() + "/lysterr/image.jpeg");
             PostFlowNavFragment.notifyStepComplete(this, data);
         } else {
             UiUtil.showToast(R.string.error_post_image_load);
@@ -94,7 +123,16 @@ public class PostFlowImageFragment extends Fragment {
                         Uri.fromFile(photoFile));
                 getParentFragment().startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
+        } else {
+            UiUtil.showToast(R.string.error_no_camera_available);
         }
+    }
+
+    private void dispatchPickPictureIntent() {
+        Intent pickPictureIntent = new Intent();
+        pickPictureIntent.setType("image/*");
+        pickPictureIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(pickPictureIntent, "Select Picture"), REQUEST_IMAGE_PICKER);
     }
 
     private File createImageFile() throws IOException {
